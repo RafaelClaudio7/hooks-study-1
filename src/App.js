2b2134a9ae38aafa42f8a9f2cee4de841286b989
RafaelClaudio7 <rc7true@gmail.com>
2022-05-30 19:39:31 -0300

    Firs day studying hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // Declaring a new variable of state, called by "count"
+  const [count, setCount] = useState(0);  // the only arg for useState is the initial state
+  // Another State variables
+  const [age, setAge] = useState(42);
+  const [fruit, setFruit] = useState('banana');
+  const [todos, setTodos] = useState([{ text: 'Learn Hooks'}]);
+
+  // Use effect, similar to ComponentDidMount update and unmount
+  
+  //Update the doc title  using browser API
+  document.title = `You clicked ${count} times on button`;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{todos[0].text}</h2>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click Here  
+      </button>    
     </div>
   );
 }
